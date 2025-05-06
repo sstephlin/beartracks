df3d59552cc5dcf17ecdf3ce68a86f39e128c0b6
bluemango23 <sue_an_park@brown.edu>
2025-05-06 15:41:37 -0400

    style: deleted unused functions

--- server/src/main/java/edu/brown/cs/student/main/server/handlers/CheckPrereqsHandler.java
+++ server/src/main/java/edu/brown/cs/student/main/server/handlers/CheckPrereqsHandler.java
@@ -96,37 +96,6 @@ public class CheckPrereqsHandler implements Route {
     this.catalog = catalog;
   }
 
-  //  @Override
-  //  public Object handle(Request request, Response response) throws Exception {
-  //    String uid = request.queryParams("uid");
-  //    String courseCode = request.queryParams("code");
-  //    String term = request.queryParams("term");
-  //    String year = request.queryParams("year");
-  //    String semesterKey = term + " " + year;
-  //
-  //    // Build completed courses set for this user up to *before* semesterKey
-  //    Map<String, List<String>> semMap = storageHandler.getAllSemestersAndCourses(uid);
-  //    System.out.println("semMap" + semMap);
-  //    Set<String> completed =
-  //        AddCourseHandlerHelper.getCompletedCourses(
-  //            semMap, semesterKey);
-  //    System.out.println("completed:" + completed);
-  //    Map<String, String> courseToSemester = new HashMap<>();
-  //    for (Map.Entry<String, List<String>> entry : semMap.entrySet()) {
-  //      for (String c : entry.getValue()) {
-  //        courseToSemester.put(c.toUpperCase(), entry.getKey());
-  //      }
-  //    }
-  //    boolean met =
-  //        AddCourseHandlerHelper.checkPrerequisites(
-  //            catalog, courseCode, completed, semesterKey, courseToSemester);
-  //
-  //    Map<String, Object> out = new HashMap<>();
-  //    out.put("response_type", "success");
-  //    out.put("prereqsMet", met); // NEW
-  //    response.type("application/json");
-  //    return Utils.toMoshiJson(out);
-  //  }
   @Override
   public Object handle(Request request, Response response) throws Exception {
     String uid = request.queryParams("uid");
@@ -146,8 +115,6 @@ public class CheckPrereqsHandler implements Route {
     boolean prereqsMet =
         AddCourseHandlerHelper.checkPrerequisites(
             catalog, courseCode, semesterKey, courseToSemester);
-    this.storageHandler.updatePrereqsMet(uid, semesterKey, courseCode, prereqsMet);
-
     this.storageHandler.updatePrereqsMet(uid, semesterKey, courseCode, prereqsMet);
 
     Map<String, Object> out = new HashMap<>();
