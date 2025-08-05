d9034e8119a9ae11e374ea289d0830df0ade41aa
Sue An Park <sue_an_park@brown.edu>
2025-08-05 11:36:35 +0900

    merging with main

--- server/src/main/java/edu/brown/cs/student/main/server/storage/FirebaseUtilities.java
+++ server/src/main/java/edu/brown/cs/student/main/server/storage/FirebaseUtilities.java
@@ -7,9 +7,10 @@ import com.google.cloud.firestore.*;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
 
@@ -22,11 +23,11 @@ public class FirebaseUtilities implements StorageInterface {
     //    Path firebaseConfigPath =
     //        Paths.get(workingDirectory, "src", "main", "resources", "firebase_config.json");
 
-    System.out.println("Checking for Firebase config file...");
-    FileInputStream serviceAccount = new FileInputStream("/etc/secrets/firebase_config.json");
-    File file = new File("/etc/secrets/firebase_config.json");
-    System.out.println("Exists: " + file.exists());
-    System.out.println("Readable: " + file.canRead());
+    String workingDirectory = System.getProperty("user.dir");
+    Path firebaseConfigPath =
+        Paths.get(workingDirectory, "src", "main", "resources", "firebase_config.json");
+
+    FileInputStream serviceAccount = new FileInputStream(firebaseConfigPath.toString());
 
     FirebaseOptions options =
         new FirebaseOptions.Builder()
@@ -34,8 +35,22 @@ public class FirebaseUtilities implements StorageInterface {
             .build();
 
     FirebaseApp.initializeApp(options);
-    System.out.println("Apps initialized: " + FirebaseApp.getApps().size());
     this.db = FirestoreClient.getFirestore();
+
+    // System.out.println("Checking for Firebase config file...");
+    // FileInputStream serviceAccount = new FileInputStream("/etc/secrets/firebase_config.json");
+    // File file = new File("/etc/secrets/firebase_config.json");
+    // System.out.println("Exists: " + file.exists());
+    // System.out.println("Readable: " + file.canRead());
+
+    // FirebaseOptions options =
+    //     new FirebaseOptions.Builder()
+    //         .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+    //         .build();
+
+    // FirebaseApp.initializeApp(options);
+    // System.out.println("Apps initialized: " + FirebaseApp.getApps().size());
+    // this.db = FirestoreClient.getFirestore();
   }
 
   @Override
